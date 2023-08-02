1de3564d7992ce469f21ff001e54c68a5e1fd770
subhajitdas1999 <sd651846@gmail.com>
2023-08-03 01:47:10 +0530

    added auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoModule } from './video/video.module';
 import { ImagesModule } from './images/images.module';
-import { AwsModule } from './aws/aws.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [VideoModule, ImagesModule, UserModule],
+  imports: [VideoModule, ImagesModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
