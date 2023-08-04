05afe9272405abf1ff96b5dfcef11dd495bc95ab
subhajitdas1999 <sd651846@gmail.com>
2023-08-05 01:36:36 +0530

    updated error message

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -14,7 +14,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException(
+        'Please provide correct email or password',
+      );
     }
     return user;
   }
