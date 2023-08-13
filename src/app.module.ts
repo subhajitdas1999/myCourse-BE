00c62dbb68a100ac5b3bb4899d9be5bc2ac18928
subhajitdas1999 <sd651846@gmail.com>
2023-08-14 02:06:57 +0530

    added payment module and logging middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoModule } from './video/video.module';
@@ -6,9 +6,15 @@ import { ImagesModule } from './images/images.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PaymentModule } from './payment/payment.module';
+import { LoggingMiddleware } from './logging/logging.middleware';
 @Module({
-  imports: [VideoModule, ImagesModule, UserModule, AuthModule],
+  imports: [VideoModule, ImagesModule, UserModule, AuthModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
