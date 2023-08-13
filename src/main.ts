36c9fd66a7c826f038d831742ff3df9dd27be97e
subhajitdas1999 <sd651846@gmail.com>
2023-08-14 02:07:48 +0530

    enabled cors, and validation transform

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true, // with this we no longer have to explicitly specific types in @Type() decorator // see pagination-query.dto.ts
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
