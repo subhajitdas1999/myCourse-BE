d5803c351de8ad1205f07253c08e569c25dffb0d
subhajitdas1999 <sd651846@gmail.com>
2023-08-18 01:07:03 +0530

    fetching jwt tokens from cookies

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,12 +1,21 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          const data = request?.cookies['jwt'];
+          if (!data) {
+            return null;
+          }
+          return data;
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
