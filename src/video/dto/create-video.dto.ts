b39f597dd3337a8c3ed5ac708f0b3790fa8a980f
subhajitdas1999 <sd651846@gmail.com>
2023-08-24 00:57:38 +0530

    Initial create video dto

--- src/video/dto/create-video.dto.ts
+++ src/video/dto/create-video.dto.ts
@@ -1,1 +1,10 @@
-export class CreateVideoDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateVideoDto {
+  @IsNotEmpty()
+  title: string;
+  @IsNotEmpty()
+  videoNo: number;
+  @IsNotEmpty()
+  courseId: string;
+}
