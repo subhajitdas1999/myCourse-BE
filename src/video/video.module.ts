145934c6a6fa548f1bee168280eb85f560645f48
subhajitdas1999 <sd651846@gmail.com>
2023-08-24 00:58:17 +0530

    Added prismaservice

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { VideoService } from './video.service';
 import { VideoController } from './video.controller';
 import { AwsModule } from 'src/aws/aws.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [AwsModule],
   controllers: [VideoController],
-  providers: [VideoService],
+  providers: [VideoService, PrismaService],
 })
 export class VideoModule {}
